d1746b22105e6ac17d50bfdc48e3f580ecedecbc
Eswar1289 <abhieswar143@gmail.com>
2023-11-23 16:38:00 +0530

    first first

--- hero.java
+++ hero.java
@@ -1,11 +1,8 @@
 // Java program to reverse a number 
 // and find if it is a palindrome or not 
 
-// Driver Class 
 class GFG { 
-	// Iterative function to 
-	// reverse the digits of number 
-	static int reversNumber(int n) 
+		static int reversNumber(int n) 
 	{ 
 		int reversed_n = 0; 
 		while (n > 0) { 
@@ -15,15 +12,11 @@ class GFG {
 		return reversed_n; 
 	} 
 
-	// Main function 
-	public static void main(String[] args) 
+		public static void main(String[] args) 
 	{ 
 		int n = 123464321; 
 		int reverseN = reversNumber(n); 
 		System.out.println("Reverse of n = " + reverseN); 
-
-		// Checking if n is same 
-		// as reverse of n 
 		if (n == reverseN) 
 			System.out.println("Palindrome = Yes"); 
 		else
